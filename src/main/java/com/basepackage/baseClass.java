af24edfcc2570995d234f3203c39ae90423eda42
Sachin <sachinbaviskar2117@gmail.com>
2025-12-11 16:49:24 +0530

    Orange hrm first commit

---
+++ src/main/java/com/basepackage/baseClass.java
@@ -0,0 +1,44 @@
+package com.basepackage;
+
+import java.time.Duration;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
+public class baseClass {
+    protected WebDriver driver;
+
+    @BeforeMethod
+    @Parameters({"browser"})
+    public void setUp(String browser) {
+        if (browser == null || browser.isEmpty()) {
+            browser = "chrome"; // default
+        }
+
+        if (browser.equalsIgnoreCase("chrome")) {
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            driver = new FirefoxDriver();
+        } else if (browser.equalsIgnoreCase("edge")) {
+            driver = new EdgeDriver();
+        } else {
+            throw new IllegalArgumentException("Unsupported browser: " + browser);
+        }
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
---
+++ src/test/java/LoginTest/Logintest.java
@@ -0,0 +1,22 @@
+package LoginTest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import com.basepackage.baseClass;
+
+public class Logintest extends baseClass {
+	
+	
+	
+		@Test
+	    public void login() {
+	        // setUp() already ran and driver is ready
+	        driver.findElement(By.name("username")).sendKeys("Admin");
+	        driver.findElement(By.name("password")).sendKeys("admin123");
+	        driver.findElement(By.xpath("//button[@type='submit']")).click();
+	        // tearDown() will run automatically after method
+	    }
+}
